8b5388671dbfc3b54ae66e7ae3438095e9cf29a3
Drew Harry <drew.harry@gmail.com>
2011-12-09 17:13:24 -0500

    First pass at io.js. Probably doesn't work at all - installing rabbitmq now to test. But framework code for starting the node and setting up appropriate exchanges and queues.

---
+++ io.js
@@ -0,0 +1,64 @@
+// io.js - A thin io layer for socket.io.
+//
+// Holds socket.io connections and relays messages to a logic core over
+// AMQP.
+
+var app = require('express').createServer(),
+    io = require('socket.io').listen(app),
+    logger = require('winston'),
+    program = require('commander'),
+    amqp = require('amqp');
+
+
+program.version('0.1')
+    .option('-v, --verbose', 'Turns on verbose logging.')
+    .option('-p, --port', 'Set the websocket port to bind to (default 8080)')
+    .parse(process.argv);
+
+
+var setLevel = "info";
+if(program.verbose) {
+    setLevel = "debug";
+}
+
+var logger = new (winston.Logger)({
+    transports: [
+      new (winston.transports.Console)({timestamp:true, level:setLevel, colorize:true}),
+    ]
+  });
+logger.setLevels(winston.config.syslog.levels);
+
+
+var queueHost = "localhost";
+if(program.args.length>=0) {
+    queueHost = program.args[0];
+} else if (program.args.length==0) {
+    logger.warn("Default to localhost for queue host.");
+}
+
+var port = 8080;
+if(program.port) {
+    port = program.port
+    logger.info("Setting port to " + program.port);
+}
+
+app.listen(port);
+
+var connection = amqp.createConnection({host: queueHost});
+
+// When we get a connection to the queue, do basic setup work.
+var exchange;
+var queue;
+connection.on('ready', function() {
+    var e = connection.exchange('socket-events',{"type":"topic"}, function(newExchange) {
+        logger.info("Exchange " + exchange.name + " is open for business.");
+        
+        exchange = newExchange;
+    });
+
+    var q = connection.queue("", {"exclusive":true}, function(newQueue) {
+        logger.info("Queue " + newQueue.name + " is open for business.");
+        
+        queue = newQueue;
+    });
+});
